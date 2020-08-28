ecd55196fb493b0868ecec0bba445005393b2c3c
faiz <ansarifaiz32@gmail.com>
2020-08-28 23:03:01 +0530

    Minor U.I changes

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
-    return ( <h1 className='title'>The Essentials Shop</h1> );
-}
+const Header = () => <h1 className='title'>The Essentials Shop</h1> ;
  
 export default Header;
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Products = props => {
     return ( 
-        <div className='col-12 col-md-6 '>
+        <div className='col-12 col-md-6 mt-1'>
             <div className="card">                    
                 <div className="card-body">
                     <h5 className="card-title">{props.data.name}</h5>
